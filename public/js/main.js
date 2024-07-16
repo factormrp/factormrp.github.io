3958bcdd826eb9c632c77fe030529dab66e0bfba
factormrp <factormrp@gmail.com>
2024-07-15 22:03:38 -0700

    Base commit

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+let inactivityTimer;
+const navItems = document.querySelectorAll('.nav-item');
+
+function startAnimation() {
+    navItems.forEach((item, index) => {
+        item.classList.add('animate');
+        item.style.animationDelay = `${index * 0.2}s`;
+    });
+}
+
+function stopAnimation() {
+    navItems.forEach(item => {
+        item.classList.remove('animate');
+        item.style.animationDelay = '';
+    });
+}
+
+function resetInactivityTimer() {
+    clearTimeout(inactivityTimer);
+    stopAnimation();
+    inactivityTimer = setTimeout(startAnimation, 15000);
+}
+
+// Event listeners
+document.addEventListener('mousemove', resetInactivityTimer);
+document.addEventListener('keypress', resetInactivityTimer);
+document.addEventListener('click', resetInactivityTimer);
+
+// Initial setup
+resetInactivityTimer();
