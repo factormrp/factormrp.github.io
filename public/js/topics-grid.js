b4af51ec71e62aedc2f05087ab555366155d6723
factormrp <factormrp@gmail.com>
2025-01-07 19:32:21 -0800

    timeline

--- public/js/topics-grid.js
+++ public/js/topics-grid.js
@@ -36,29 +36,35 @@ const getImageBlob = async (url) => {
   return imageResponse.blob();
 };
 
-const createGalleryFigure = async (galleryImage) => {
+const timeline = document.querySelector('.timeline');
+
+const createTimelineItem = async (galleryImage, index) => {
   try {
     const imageBlob = await getImageBlob(galleryImage.url);
-    const myDiv = document.createElement('div');
-    myDiv.setAttribute('class','topic-card');
-    const myImage = document.createElement('img');
-    myImage.src = window.URL.createObjectURL(imageBlob);
-    myImage.setAttribute('alt', galleryImage.alt);
-    const myName = document.createElement('h3');
-    myName.textContent = `${galleryImage.name}`;
-    myName.setAttribute('class', 'topic-name');
-    const myDate = document.createElement('p');
-    myDate.textContent = `${galleryImage.date}`
-    myDate.setAttribute('class', 'topic-date');
-    const myVenue = document.createElement('p');
-    myVenue.textContent = `${galleryImage.venue}`
-    myVenue.setAttribute('class', 'topic-venue');
-    myDiv.append(myImage, myName, myDate, myVenue);
-    grid.append(myDiv);
+    const timelineItem = document.createElement('div');
+    timelineItem.setAttribute(
+      'class', `timeline-item ${index % 2 === 0 ? 'left' : 'right'}`
+    );
+    timelineItem.addEventListener(
+      'click', () => {timelineItem.classList.toggle('expanded');}
+    );
+
+    const content = `
+      <div class="timeline-content">
+        <img class="timeline-image hidden" src="${window.URL.createObjectURL(imageBlob)}" alt="${galleryImage.alt}">
+        <h3 class="timeline-name">${galleryImage.name}</h3>
+        <p class="timeline-date">${galleryImage.date}</p>
+        <p class="timeline-blurb hidden">${galleryImage.blurb}</p>
+      </div>
+    `;
+
+    timelineItem.innerHTML = content;
+    timeline.appendChild(timelineItem);
   } catch (error) {
     console.error(error);
   }
 };
 
 registerServiceWorker();
-Gallery.images.map(createGalleryFigure);
+// Gallery.images.map(createGalleryFigure);
+Gallery.images.sort((a,b) => a.date - b.date).forEach((image, index) => createTimelineItem(image, index));
